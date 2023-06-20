d887c01ea67df4a0dcdd5c1e72d8bd1c391526f2
Wilson <wilsonwuuu@gmail.com>
2023-06-20 12:58:04 -0700

    Fixed converter transition

--- src/Converter.js
+++ src/Converter.js
@@ -14,6 +14,8 @@ const Converter = (props) => {
     
     
     const currencySymbols = ["AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN","BAM","BBD","BDT","BGN","BHD","BIF","BMD","BND","BOB","BRL","BSD","BTN","BWP","BYN","BZD","CDF","CHF","CLF","CLP","CNH","CNY","COP","CRC","CUC","CUP","CVE","CZK","DJF","DKK","DOP","DZD","EGP","ERN","ETB","EUR","FJD","FKP","GBP","GEL","GGP","GHS","GIP","GMD","GNF","GTQ","GYD","HKD","HNL","HRK","HTG","HUF","IDR","ILS","IMP","INR","IQD","IRR","ISK","JEP","JMD","JOD","JPY","KES","KGS","KHR","KMF","KPW","KRW","KWD","KYD","KZT","LAK","LBP","LKR","LRD","LSL","LYD","MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRU","MUR","MVR","MWK","MXN","MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON","RSD","RUB","RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP","SLL","SOS","SRD","SSP","STD","STN","SVC","SYP","SZL","THB","TJS","TMT","TND","TOP","TRY","TTD","TWD","TZS","UAH","UGX","USD","UYU","UZS","VES","VND","VUV","WST","XAF","XAG","XAU","XCD","XDR","XOF","XPD"];
+    
+    /*
     // Add currencies to the list of currency types
     function addCurrenciesToList(){
         // Get a reference to each select element
@@ -32,6 +34,7 @@ const Converter = (props) => {
                 selectElementTo.appendChild(newOption1);
         }
     }
+    */
 
     function convertNumberToLocaleString(val){
         const result = Number(val).toLocaleString(undefined, {
--- src/Historical.js
+++ src/Historical.js
@@ -2,18 +2,31 @@ import {useState, useEffect} from 'react';
 const Historical = (histProp) => {
     
     const [histData, setHistData] = useState(null);
-    const [histCryptoData, setHistCryptoData] = useState(null);
     const [currentData, setCurrentData] = useState(null);
+    const [histCryptoData, setHistCryptoData] = useState(null);
     const [currentCryptoData, setCurrentCryptoData] = useState(null);
-
+    
 
     const currencySymbols = ["AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN","BAM","BBD","BDT","BGN","BHD","BIF","BMD","BND","BOB","BRL","BSD","BTN","BWP","BYN","BZD","CDF","CHF","CLF","CLP","CNH","CNY","COP","CRC","CUC","CUP","CVE","CZK","DJF","DKK","DOP","DZD","EGP","ERN","ETB","EUR","FJD","FKP","GBP","GEL","GGP","GHS","GIP","GMD","GNF","GTQ","GYD","HKD","HNL","HRK","HTG","HUF","IDR","ILS","IMP","INR","IQD","IRR","ISK","JEP","JMD","JOD","JPY","KES","KGS","KHR","KMF","KPW","KRW","KWD","KYD","KZT","LAK","LBP","LKR","LRD","LSL","LYD","MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRU","MUR","MVR","MWK","MXN","MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON","RSD","RUB","RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP","SLL","SOS","SRD","SSP","STD","STN","SVC","SYP","SZL","THB","TJS","TMT","TND","TOP","TRY","TTD","TWD","TZS","UAH","UGX","USD","UYU","UZS","VES","VND","VUV","WST","XAF","XAG","XAU","XCD","XDR","XOF","XPD"];
+    const rates24Hr = [];
+    async function calculate24HrRates(){
 
+        for (let key in histProp.savedRates) {
+            
+            if (histProp.savedRates.hasOwnProperty(key)) {
+                const value = histProp.savedRates[key];
+                const histValue = histProp.historicalData[key];
+                console.log( value, histValue, 100 * ((value/histValue) - 1));
+                rates24Hr.push(100 * ((value/histValue) - 1));
+            }
+          }
+    }
     
     useEffect(() => {
         setHistData(histProp.historicalData);
         setCurrentData(histProp.savedRates);
         setCurrentCryptoData(histProp.savedCryptoRates);
+        calculate24HrRates();
         
     }, []);
 
@@ -35,15 +48,13 @@ const Historical = (histProp) => {
                 </ul>
                     
                     <ul className="historical_box_lists_24">
-                        <li>Item 4</li>
-                        <li>Item 5</li>
-                        <li>Item 6</li>
+                        {rates24Hr.map((symbol) => (
+                        <li>{symbol}</li>
+                        ))}
                     </ul>
                     
                     <ul className="historical_box_lists_month">
-                        <li>Item 7</li>
-                        <li>Item 8</li>
-                        <li>Item 9</li>
+                        
                     </ul>
                 </div>
             
