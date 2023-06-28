1d66c2da78c21d6b433013629fe0936b3e60221e
Wilson <wilsonwuuu@gmail.com>
2023-06-27 19:44:31 -0700

    Fix mobile view to support prices, and %changes

--- src/Historical.js
+++ src/Historical.js
@@ -12,7 +12,6 @@ const Historical = (props) => {
     function changeDenominationCurrency(){
         var tempArray = [];
         var toCurrency = document.getElementById("historical_description_changeCurrency").value;
-        
         if(props.savedRates.hasOwnProperty(toCurrency)){
             setCurrentCurrency(toCurrency);
             for(var i = 0; i < props.savedCryptoRates.length; i++){
@@ -30,6 +29,74 @@ const Historical = (props) => {
         setCryptoPriceInCurrency(tempArray);
     }
 
+    function changeView() {
+            var selectedView = document.getElementById("historical_description_changeView").value;
+            var elementPriceLabel = document.getElementById("historical_box_labels_price");
+            var element24hLabel = document.getElementById("historical_box_labels_24h");
+            var element30dLabel = document.getElementById("historical_box_labels_30d");
+            var elementPriceDiv = document.getElementsByClassName("historical_box_lists_container_price");
+            var element24hDiv = document.getElementsByClassName("historical_box_lists_container_24h");
+            var element30dDiv = document.getElementsByClassName("historical_box_lists_container_30d");
+        
+            if (selectedView === "Price") {
+                elementPriceLabel.style.display = 'block';
+                element24hLabel.style.display = 'none';
+                element30dLabel.style.display = 'none';
+            } else if (selectedView === "24h") {
+                elementPriceLabel.style.display = 'none';
+                element24hLabel.style.display = 'block';
+                element30dLabel.style.display = 'none';
+            } else {
+                elementPriceLabel.style.display = 'none';
+                element24hLabel.style.display = 'none';
+                element30dLabel.style.display = 'block';
+            }
+
+            for (var i = 0; i < elementPriceDiv.length; i++) {
+                if (selectedView === "Price") {
+                    elementPriceDiv[i].style.display = 'block';
+                    element24hDiv[i].style.display = 'none';
+                    element30dDiv[i].style.display = 'none';
+                } else if (selectedView === "24h") {
+                    elementPriceDiv[i].style.display = 'none';
+                    element24hDiv[i].style.display = 'block';
+                    element30dDiv[i].style.display = 'none';
+                } else {
+                    elementPriceDiv[i].style.display = 'none';
+                    element24hDiv[i].style.display = 'none';
+                    element30dDiv[i].style.display = 'block';
+                }
+            }
+      }
+
+      function changeViewFull() {
+        console.log("here");
+        
+        var elementPriceLabel = document.getElementById("historical_box_labels_price");
+        var element24hLabel = document.getElementById("historical_box_labels_24h");
+        var element30dLabel = document.getElementById("historical_box_labels_30d");
+        var elementPriceDiv = document.getElementsByClassName("historical_box_lists_container_price");
+        var element24hDiv = document.getElementsByClassName("historical_box_lists_container_24h");
+        var element30dDiv = document.getElementsByClassName("historical_box_lists_container_30d");
+    
+
+        elementPriceLabel.style.display = 'block';
+        element24hLabel.style.display = 'block';
+        element30dLabel.style.display = 'block';
+
+
+        for (var i = 0; i < elementPriceDiv.length; i++) {
+           
+                elementPriceDiv[i].style.display = 'block';
+                element24hDiv[i].style.display = 'block';
+                element30dDiv[i].style.display = 'block';
+
+        }
+  }
+
+    
+
+
     function convertNumberToLocaleString(val){
         const result = Number(val).toLocaleString(undefined, {
             minimumFractionDigits: 0,
@@ -37,6 +104,30 @@ const Historical = (props) => {
         });
         return result;
     }
+
+    useEffect(() => {
+        function handleResize() {
+          const screenWidth = window.innerWidth;
+          const threshold = 992; // Set your desired threshold here
+    
+          if (screenWidth > threshold) {
+            // Call your function here
+            changeViewFull();
+          }
+          else{
+            changeView();
+          }
+        }
+    
+        // Attach the event listener for window resize
+        window.addEventListener('resize', handleResize);
+    
+        // Clean up the event listener when the component unmounts
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
+
 
 
     return (  
@@ -55,13 +146,20 @@ const Historical = (props) => {
                         <option value={symbol}>{symbol}</option>
                         ))}
                 </select> 
+
+                <select className = "historical_description_changeView"  id="historical_description_changeView" onChange={changeView}>
+                        <option value ="Price">Price</option>
+                        <option value ="24h">24h</option>
+                        <option value ="30d">30d</option>
+                </select> 
             </div>
+
             <div className = "historical_box">
-                <div className = "historical_box_labels">
-                    <label className = "converter_label_value">Coin</label>
-                    <label className = "converter_label_value">Price</label>
-                    <label className = "converter_label_from">24h</label>
-                    <label className = "converter_label_to">30d</label>
+                <div className = "historical_box_labels" id = "historical_box_labels">
+                    <label className = "historical_box_labels_coin">Coin</label>
+                    <label className = "historical_box_labels_price" value = "Price" id = "historical_box_labels_price">Price</label>
+                    <label className = "historical_box_labels_24h" value = "24h" id = "historical_box_labels_24h">24h</label>
+                    <label className = "historical_box_labels_30d" value = "30d" id = "historical_box_labels_30d">30d</label>
                     
                 </div>
                 
@@ -75,11 +173,11 @@ const Historical = (props) => {
                             </div>
 
 
-                            <div className="historical_box_lists_container_price">
+                            <div className="historical_box_lists_container_price" id = "historical_box_lists_container_price">
                                 {convertNumberToLocaleString(cryptoPriceInCurrency[index])}
                             </div>
 
-                            <div className="historical_box_lists_container_24h">
+                            <div className="historical_box_lists_container_24h" id = "historical_box_lists_container_24h">
                                 {props.savedCryptoRates[key].price_change_percentage_24h_in_currency < 0 ? 
                                 (
                                     <span className="negative_value_24h" style={{color: '#ea3943'}}>
@@ -95,7 +193,7 @@ const Historical = (props) => {
                                 }
                             </div>
 
-                            <div className="historical_box_lists_container_30d">
+                            <div className="historical_box_lists_container_30d" id = "historical_box_lists_container_30d">
                                 {props.savedCryptoRates[key].price_change_percentage_30d_in_currency < 0 ? 
                                 (
                                     <span className="negative_value_30d" style={{color: '#ea3943'}}>
